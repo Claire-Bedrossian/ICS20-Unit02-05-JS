3b6814c4c312cb45067f22e1cf0e318322fc36b4
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-18 18:19:17 +0000

    Added a function

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
+//
+// Created by: Claire Bedrossian
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates the user pay and tax deductions
+ */
+function myButtonClicked() {
+  // input
+  const hours = parseFloat(document.getElementById('hours-worked').value)
+  const hourlyPay = parseFloat(document.getElementById('hourly-pay').value)
+  const TAX_RATE = 0.18
+
+  //process
+
+  const grossPay = hours * hourlyPay
+  const deductions = TAX_RATE * grossPay
+  const afterDeductions = grossPay - (TAX_RATE * grossPay)
+
+  // output
+  document.getElementById('grossPay').innerHTML = `Your pay will be: $ ${grossPay.toFixed(2)}`
+  document.getElementById('deductions').innerHTML = `The governement will take: $ ${deductions.toFixed(2)}`
+  document.getElementById('afterDeductions').innerHTML = `Your pay after deductions is: $ ${afterDeductions.toFixed(2)}`
+}
