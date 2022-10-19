890d191dae539b9f663028c269b369dea2b392f3
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-10-19 17:44:03 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -10,18 +10,24 @@
  */
 function myButtonClicked() {
   // input
-  const hours = parseFloat(document.getElementById('hours-worked').value)
-  const hourlyPay = parseFloat(document.getElementById('hourly-pay').value)
+  const hours = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyPay = parseFloat(document.getElementById("hourly-pay").value)
   const TAX_RATE = 0.18
 
   //process
 
   const grossPay = hours * hourlyPay
   const deductions = TAX_RATE * grossPay
-  const afterDeductions = grossPay - (TAX_RATE * grossPay)
+  const afterDeductions = grossPay - TAX_RATE * grossPay
 
   // output
-  document.getElementById('grossPay').innerHTML = `Your pay will be: $ ${grossPay.toFixed(2)}`
-  document.getElementById('deductions').innerHTML = `The governement will take: $ ${deductions.toFixed(2)}`
-  document.getElementById('afterDeductions').innerHTML = `Your pay after deductions is: $ ${afterDeductions.toFixed(2)}`
+  document.getElementById(
+    "grossPay"
+  ).innerHTML = `Your pay will be: $ ${grossPay.toFixed(2)}`
+  document.getElementById(
+    "deductions"
+  ).innerHTML = `The governement will take: $ ${deductions.toFixed(2)}`
+  document.getElementById(
+    "afterDeductions"
+  ).innerHTML = `Your pay after deductions is: $ ${afterDeductions.toFixed(2)}`
 }
